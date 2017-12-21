379e96e6a9399f076423f9b60d8de25387b226fb
GertJMyburgh <gertjmyburgh@gmail.com>
2017-12-21 21:35:42 +0200

    Busy merging Michelle's mockup with my real site.

---
+++ funeral-cover.php
@@ -0,0 +1,233 @@
+<?php ?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="images/favicon.ico">
+
+        <title>Hollard Branch Locator</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+        <!-- <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet"> -->
+
+        <!-- Custom styles for this template -->
+        <link href="css/funeral-main.css" rel="stylesheet">
+
+        <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+        <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+        <!--<script src="../../assets/js/ie-emulation-modes-warning.js"></script> -->
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+
+    <body>
+
+        <script>            
+            
+            //alert ("index.php - Executing Google Analytics");
+            
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+            ga('create', 'UA-83544421-1', 'auto');
+            ga('send', 'pageview');
+            
+        </script>
+        
+        <!--TOP MASTHEAD CONTAINING HOME ICON LINKING BACK TO BRANCHES INDEX PG-->
+        <div class="home-masthead">
+            <div class="container">
+                <div class="row">        
+                    <nav class="home-main">			
+                        <a class="home-icon" href="index.php">&nbsp;</a> <a class="funeral-pgr" href="funeral-cover.php">Funeral</a>
+                    </nav>
+                </div>
+            </div>
+        </div>
+
+        <!--2ND MASTHEAD CONTAINING HOLLARD LOGO LINKING BACK TO BRANCHES INDEX PG-->
+        <div class="logo-masthead">
+            <div class="container">
+                <div class="">
+                    <ul class="nav-main">
+                        <li class="nav-main__logo">
+                            <a class="logo" href="index.php">&nbsp;</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>    
+
+
+        <!--BACKGROUND IMAGE AND FORM-->    
+        <div class="container-fluid"id="TOP-BGIMAGE">
+        </div>
+        <div class="row" style=" background-color:#dcd1e1; padding:0;">
+
+            <div class="container" style="padding:0;">
+
+                <div class="col-lg-6 col-md-6 branch-header branch-title">
+                    <h1>Funeral Cover</h1>
+                    <h3 class="branch-subheading">Get funeral cover from R10 000 up to R75 000</h3>
+                </div>
+
+
+
+                <div class="col-sm-6 col-sm-offset-6 col-md-6 branch-sidebar" style="float:none;">
+                    <div class="sidebar-module sidebar-module-inset sidebar">
+                        <h2 style=" font-size:20px; color:#333;">Call me back for a quote:</h2>
+                        <form action="" method="">
+                            <div class="form-group">
+                                <!--<label for="name">Name</label>-->
+                                <input type="text" class="form-control" id="name" value="Name*" />
+                            </div>
+                            <div class="form-group">                    
+                                <!--<label for="email">Email</label>-->
+                                <input type="text" class="form-control" id="phoneNumber" value="Contact Number*" />    
+                            </div>            
+                            <div class="form-group">            
+                                <input type="submit" class=" sidebar-btn" id="submit" value="CALL ME BACK"/>                       
+                            </div>
+                        </form>            
+                    </div>
+
+                </div>
+
+            </div> 
+
+
+        </div>
+
+
+
+
+        <!--FUNERAL CONTENT SECTION-->
+        <div class="container">
+
+
+
+
+            <div class="row" style="padding-bottom: 15px;">
+
+                <div class="col-sm-12 col-md-6 col-lg-6 branch-main">
+
+                    <p class="funeral-info" style="margin-top: 30px;">Hollard Funeral Plan will help you prepare for tomorrow, 
+                        because tomorrow is not promised to us.
+                        Planning a funeral for someone you love is hard enough, without the added stress of worrying about how to pay for it.
+                        Give your family the support they need when you're no 
+                        longer around with quality funeral insurance from Hollard.
+                        This funeral plan will give your loved ones a lump sum to cover funeral costs, as well as the extra support they'll need. 
+                    </p> 
+
+                    <h3>Benefits of a Hollard funeral plan include:</h3>
+
+                    <ul>
+                        <li class="hld-li-tick">Cover from R10 000 to R75 000 depending on your needs an budget.</li>
+                        <li class="hld-li-tick">A pay-out within 24 hours to cover funeral expenses. After we received required documentations for a valid claim.</li>
+                        <li class="hld-li-tick">Free use of a rental vehicle with full tank and unlimited mileage for 3 days to help with the funeral arrangement.</li>
+                        <li class="hld-li-tick">A portion of your premiums back at claim stage, when either you or your partner pass on.</li>
+                        <li class="hld-li-tick">R35 000 towards your children university fees, when either you when either you.</li>
+                        <li class="hld-li-tick">An Immediate Accidental Death Benefit so you're covered as soon as we get your first premium.</li>
+                        <li class="hld-li-tick">You can cover up to 22 family members on one plan.</li>
+                    </ul>
+
+
+                    <!--END OF Left side-->
+
+
+                </div>
+
+                <!--RIGHT SIDE-->
+
+                <div class="col-sm-6 branch-sidebar">
+
+                    <h3 style="margin-top: 30px;">Optional Extra Benefits:</h3>
+
+                    <ul>
+                        <li class="hld-li-tick" style="font-weight:bold;">Extended Family Funeral Rider<br>
+                            <span class="sublist-bullet">&bull; Funeral cover for your partner, children, parents and other blood relatives.</span></li>
+                        <li class="hld-li-tick" style="font-weight:bold;">Monthly Provider<br>
+                            <span class="sublist-bullet">&bull; Cover of up to R20 000 for your family's monthly expenses for a year after you pass away.</span></li>
+                        <li class="hld-li-tick" style="font-weight:bold;">Moneyback Benefit<br>
+                            <span class="sublist-bullet">&bull; 20% cash back after every five years of paying all your insurance policy premiums.</span></li>
+                        <li class="hld-li-tick" style="font-weight:bold;">Memorial Benefit<br>
+                            <span class="sublist-bullet">&bull; Cover the cost (either R5 000 or R10 000) of an unveiling ceremony.</span></li>
+                        <li class="hld-li-tick" style="font-weight:bold;">Life Cover Benefit<br>
+                            <span class="sublist-bullet">&bull; Up to R120 000 lump sum when you or your insured partner passes away.</span></li>
+                        <li class="hld-li-tick" style="font-weight:bold;">Personal Accident Benefit<br>
+                            <span class="sublist-bullet">&bull; Up to R100 000 cover if you become disabled or die accidentally.</span></li>
+                    </ul>
+                </div>
+
+            </div>
+
+
+        </div><!-- /.branch-sidebar -->
+
+    </div><!-- /.row -->
+
+</div><!-- /.container -->
+
+<!--FOOTER-->
+<footer class="branch-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4 col-sm-push-8 branch-footer-social">
+                <ul class="nav-social">
+                    <li class="nav-social--facebook">
+                        <a href="https://www.facebook.com/HollardInsurance" target="_blank"><i class="icon--facebook">&nbsp;</i></a></li>
+                    <li class="nav-social--linkedin">
+                        <a href="http://www.linkedin.com/company/12696?trk=cws-btn-overview-0-0" target="_blank"><i class="icon--linkedin">&nbsp;</i></a></li>
+                    <li class="nav-social--twitter">
+                        <a href="https://twitter.com/hollard" target="_blank"><i class="icon--twitter">&nbsp;</i></a></li>                    
+                    <li class="nav-social--googleplus">
+                        <a href="https://plus.google.com/u/0/+HollardInsurance" target="_blank"><i class="icon--googleplus">&nbsp;</i></a></li>                    
+                    <li class="nav-social--instagram">
+                        <a href="http://instagram.com/hollardgram?ref=badge" target="_blank"><i class="icon--instagram">&nbsp;</i></a></li>                    
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <p>The Hollard Insurance Company Ltd (Reg No. 1952/003004/06) is an authorised Financial Services Provider, and a member of the South African Insurance Association (SAIA).<br>
+                Hollard Life Assurance Company (Reg. No. 1993/001405/06) is an authorised Financial Services Provider and a member of the Association for Savings and Investment South Africa (ASISA).<br> 
+                Hollard Investment Managers (Reg. No. 1997/001696/07) is an authorised Financial Services Provider, operates as an investment manager and is a member of the Association for Savings and Investment South Africa (ASISA).<br>
+                <br>
+                Hollard is committed to high ethical standards of business. Hollard subscribes to the Ombudsman for Short-Term Insurance and the Ombudsman for Long-Term Insurance, and is subject to the jurisdiction of the FAIS Ombud. <br>
+                <br>
+                Hollard has developed and publicises its own Financial Crime Risk Management Policy as well as policies in support of the aims of the 
+                Anti Money-Laundering Act and the Corrupt Activities Act.
+            </p>
+            <p>
+                &copy; Copyright 2016 Hollard 
+            </p>
+        </div>
+    </div>
+</footer>
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+</body>
+</html>
